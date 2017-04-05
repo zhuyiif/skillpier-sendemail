c8b6f4bd38b1b4f689143a77d83983ca77730bb6
yi zhu <zhuyiif@gmail.com>
2017-04-05 15:16:54 -0700

    use get

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ let transporter = nodemailer.createTransport({
 
 app.use('/skillpier-action_files', express.static(path.join(__dirname + '/skillpier-action_files')))
 app.get('/', function(req, res) {
-    res.sendfile('./skillpier-action.htm');
+    //res.sendfile('./skillpier-action.htm');
+    res.sendFile(path.join(__dirname, '/', 'skillpier-action.htm'));
 });
 
 
@@ -35,6 +36,35 @@ console.log("info = ", req.params.info);
 
   res.send('Hello World')
 })
+
+app.get('/sendemailp', function(req, res) {
+ 
+
+var first = req.param('firstname');
+  var last = req.param('lastname');
+  
+
+  let mailOptions = {
+    from: mailuser, // sender address
+    to: 'administrator@skillpier.com,zhuyi8319@163.com', // list of receivers
+    subject: 'Hello Zack test', // Subject line
+    text: first + "#" + last, // plain text body
+    html: req.params.info // html body
+};
+
+// // send mail with defined transport object
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+
+     res.send('You sent the name ' + first + " last:" + last);
+});
+
+
+});
+
 
 app.post('/sendemailp', function(req, res) {
  
